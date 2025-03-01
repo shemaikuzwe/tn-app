cd7732a9d3720f25968e3a887d31d67f3c89819c
shemaikuzwe <eshemaikuzwe@gmail.com>
2025-03-01 19:07:23 +0200

    fix release

--- src/template/drizzle/drizzle/index.ts
+++ src/template/drizzle/drizzle/index.ts
@@ -4,4 +4,4 @@ import * as schema from "@/drizzle/index.ts";
 const db = drizzle(process.env.DATABASE_URL!, { schema });
 type User = typeof schema.users.$inferSelect;
 
-export { db, User };
+export { db,type User };
